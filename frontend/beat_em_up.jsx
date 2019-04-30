9c30a2cb58684598d3de166ee8e2ce86f71bf1b0
Samuel Inglese <senglese96@gmail.com>
2019-04-30 16:57:09 -0700

    finish most of connecting backend auth to frontend auth

--- frontend/beat_em_up.jsx
+++ frontend/beat_em_up.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+import configureStore from './store/store'
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>It's a start!</h1>, root)
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    ReactDOM.render(<h1>It's a start!</h1>, root);
 })
